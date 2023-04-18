4ad117118c7c9f7a3ab4be8c61658d800e8f0eff
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-04-18 14:23:18 -0500

    add calculation created

---
+++ script.js
@@ -0,0 +1,17 @@
+//addition calculation
+
+const add = function(a,b){
+    let answer = a + b;
+    return answer
+}
+
+add(3,2);
+
+//subtract calculation
+
+
+
+//multiply calculation
+
+
+//divide calculation
